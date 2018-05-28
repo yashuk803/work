772561812fcf551ee13c9826874f2bef6cf77954
Mari <yashuk803@gmail.com>
2018-05-28 09:23:14 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var page ={
+  		init:function() {
+  			var self = this;
+  		
+  			$('#collapseExample').on('hidden.bs.collapse', function () {
+ 						$('.tg-class i').toggleClass("fa-angle-down fa-angle-up");
+				})
+  			$('#collapseExample').on('show.bs.collapse', function () {
+ 						$('.tg-class i').toggleClass("fa-angle-down fa-angle-up");
+				})
+  			$('#rating').on('hidden.bs.collapse', function () {
+ 						$('.tg-servis i').toggleClass("fa-chevron-down fa-chevron-up");
+				})
+  			$('#rating').on('show.bs.collapse', function () {
+ 						$('.tg-servis i').toggleClass("fa-chevron-down fa-chevron-up");
+				})
+  			$('#language').on('hidden.bs.dropdown', function () {
+ 						$('#tg-lang i').toggleClass("fa-chevron-down fa-chevron-up");
+				})
+  			$('#language').on('shown.bs.dropdown', function () {
+ 						$('#tg-lang i').toggleClass("fa-chevron-down fa-chevron-up");
+				})
+  			$(".blockRaiting").on("click", function(){
+  					self.close();
+  				})
+  			$(".card").on("click", function(){
+  				let $url = "https://gentle-peak-67938.herokuapp.com/",
+  				$score = $(this).attr("data-score"); 
+  				$.ajax({
+   	 				url: $url,        
+    				data:{score:$score},
+    				type: "POST",
+    				success: function (data) {
+    					console.log(data);
+    					
+    					self.closeAll();
+$('#thankModal').modal('show');
+    				}               
+				});  				
+  			})
+
+  		},
+  		close:function(){
+        if($(".closeBlock").css("dicplay")!="block"){
+          $(".closeBlock").hide(400, function(){
+              $(".resume").css("padding-top", 20);
+            });
+        }
+  			
+  		},
+  		closeAll: function(){
+  			this.close();
+  			$('.hidBlock').hide(400, function(){
+  						$(".resume").css("padding-top", 20);
+  					});
+
+  		}
+  		
+}
+ $( window ).on( "load", function() {
+         page.init();
+    });
